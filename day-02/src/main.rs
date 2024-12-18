2f17a6b789237fd45bc49514c9f61a393ec5dec6
Kent Deterding <109356505+KentDeterding@users.noreply.github.com>
2024-12-18 10:38:00 -0600

    completed day 02 part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -34,5 +34,52 @@ fn main() -> Result<()> {
 
     println!("Part 1: {safe_count}");
 
+    // Part 2
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let mut safe_count = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+        let report: Vec<_> = line
+            .split_whitespace()
+            .filter_map(|i| i.parse::<i32>().ok())
+            .collect();
+        
+        if check_safety(report.clone()) {
+            safe_count += 1;
+        } else {
+            for i in 0..report.len() {
+                let mut dampened_report = report.clone();
+                dampened_report.remove(i);
+
+                if check_safety(dampened_report) {
+                    safe_count += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("Part 2: {safe_count}");
+
     Ok(())
+}
+
+fn check_safety(report: Vec<i32>) -> bool {
+    let temp = report[0] - report[report.len() - 1];
+    if temp == 0 {
+        return false
+    }
+    let sign = temp / temp.abs();
+    let mut safety = true;
+
+    for i in 0..(report.len() - 1) {
+        let diff = report[i] - report[i+1];
+        if diff * sign < 1 || 3 < diff * sign {
+            safety = false;
+        }
+    }
+
+    safety
 }
